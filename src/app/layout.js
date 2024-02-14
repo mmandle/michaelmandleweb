58d20d525efa61f44de5e3b9008f98f9a943af62
MichaelMandle <m1mandle1@gmail.com>
2024-02-14 11:55:22 -0500

    vercel speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
